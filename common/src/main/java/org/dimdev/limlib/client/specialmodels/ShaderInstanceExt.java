a6113a74c55be5b5067e846db0c4b5089383c1a1
Waterpicker <Waterpickerenternity@gmail.com>
2026-07-20 16:56:12 -0500

    Removed speical models from corners.

--- common/src/main/java/org/dimdev/limlib/client/specialmodels/ShaderInstanceExt.java
+++
@@ -1,9 +0,0 @@
-package org.dimdev.limlib.client.specialmodels;
-
-import net.minecraft.client.renderer.ShaderInstance;
-
-import java.util.function.Consumer;
-
-public interface ShaderInstanceExt {
-    void addUniformSetCallback(Consumer<ShaderInstance> callback);
-}
